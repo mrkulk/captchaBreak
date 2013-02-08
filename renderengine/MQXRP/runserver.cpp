5ee21ccb50a44a74b89c7af0c612e6bac10b5c54
Tejas <tejasdkulkarni@gmail.com>
2013-02-08 06:10:31 -0500

    zeroMQ XRP code first draft

--- renderengine/MQXRP/runserver.cpp
+++ renderengine/MQXRP/runserver.cpp
@@ -9,12 +9,19 @@ int main () {
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
 
+
+    zmq::context_t context2 (1);
+    zmq::socket_t socket2 (context2, ZMQ_REP);
+    socket2.bind ("tcp://*:5551");
+
     while (true) {
         zmq::message_t request;
+        zmq::message_t request2;
 
         //  Wait for next request from client
         socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
+        socket2.recv (&request2);
+        std::cout << "Received 2 servers" << std::endl;
 
         //  Do some 'work'
         sleep (1);
@@ -23,6 +30,10 @@ int main () {
         zmq::message_t reply (5);
         memcpy ((void *) reply.data (), "World", 5);
         socket.send (reply);
+
+        zmq::message_t reply2 (5);
+        memcpy ((void *) reply2.data (), "WHATT", 5);
+        socket2.send (reply2);
     }
     return 0;
 }
